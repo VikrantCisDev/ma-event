e7fd20e985c79718050033b3206d638b84670eea
vikrantcisdev <vikrant.raj@cisinlabs.com>
2025-01-09 20:15:56 +0530

    homepage first

--- src/components/ui/ImageCard.jsx
+++ src/components/ui/ImageCard.jsx
@@ -4,11 +4,17 @@ import Card from 'react-bootstrap/Card';
 const ImageCard = ({data,cardStyle}) => {
   const styleOptions ={
     primary: "position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center justify-content-center bg-opacity-50 bg-black text-light",
-    secondary: "pink-text"
+    secondary: "pink-text text-center"
   }
   return (
     <Card className='border-0'>
-      <Card.Img variant="top" src={data.img} />
+    {cardStyle === "secondary" ? (
+      <div className="roundPinkImg">
+        <div className="icon">{data.img}</div>
+      </div>
+    ): (
+      <Card.Img variant="top" src={data.img} className='img-fluid' />
+    )}
       <Card.Body className={styleOptions[cardStyle] ? styleOptions[cardStyle] : styleOptions[secondary]}>
         <Card.Title>{data.title}</Card.Title>
         <Card.Text>{data.text}</Card.Text>
--- src/pages/HomePage/New.jsx
+++ src/pages/HomePage/New.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ImageCard from '../../components/ui/ImageCard';
 import image from "../../assets/Images/diet.png"
 import Header from '../../components/ui/header';
+
+import { AiOutlineFileSearch } from "react-icons/ai";
+import { BsShopWindow } from "react-icons/bs";
+
+
 
 const New = () => {
 
@@ -43,6 +47,33 @@ const New = () => {
     }
   ]
 
+  let cardData2 = [
+    {
+      img: <AiOutlineFileSearch className='icon'/>,
+      title: "Video/Photography",
+    },
+    {
+      img: <AiOutlineFileSearch className='icon'/>,
+      title: "Video/Photography",
+    },
+    {
+      img: <AiOutlineFileSearch className='icon'/>,
+      title: "Video/Photography",
+    },
+    {
+      img: <AiOutlineFileSearch className='icon'/>,
+      title: "Video/Photography",
+    },
+    {
+      img: <AiOutlineFileSearch className='icon'/>,
+      title: "Video/Photography",
+    },
+    {
+      img: <AiOutlineFileSearch className='icon'/>,
+      title: "Video/Photography",
+    }
+  ]
+
   return (
     <>
       <Header />
@@ -65,20 +96,26 @@ const New = () => {
       </section>
       <section className='py-5'>
         <div className='custom-container'>
-        <div className="heading-wrapper">
-          <h3 className='text-center pink-text '>Your Perfect Event with all your Vendors on one platform</h3>
+          <div className="heading-wrapper pink-text text-center mb-5">
+            <h3 className=' fs-1 mb-2 fw-normal'>How it works</h3>
+            <p className='fs-4 fw-medium'>The most efficient way to find all your event vendors in one place</p>
+          </div>
         </div>
-          <Row className='row-gap-3'>
-            {
-              cardData.map((item, index) => {
-                return (
-                  <Col lg={3} md={4} sm={6} key={index}>
-                    <ImageCard data={item} cardStyle={"secondary"} />
-                  </Col>
-                )
-              })
-            }
-          </Row>
+        <div className="howitworkbg">
+          <div className='custom-container'>
+
+            <Row className='py-5 '>
+              {
+                cardData2.map((item, index) => {
+                  return (
+                    <Col lg={2} md={4} sm={6} key={index}>
+                      <ImageCard data={item} cardStyle={"secondary"} />
+                    </Col>
+                  )
+                })
+              }
+            </Row>
+          </div>
         </div>
 
       </section>
