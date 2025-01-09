95fc5103a8d4e10157bca0985655ed5c988ff97e
vikrantcisdev <vikrant.raj@cisinlabs.com>
2025-01-09 17:48:19 +0530

    headerr

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -48,10 +48,9 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto text-uppercase">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
+                            <Nav.Link href="#home">ABout ma-event</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.1">Planning tools</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">
                                     Another action
                                 </NavDropdown.Item>
@@ -61,6 +60,21 @@ const Header = () => {
                                     Separated link
                                 </NavDropdown.Item>
                             </NavDropdown>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Vendors</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#link">latest events</Nav.Link>
+                            <Nav.Link href="#link">Images</Nav.Link>
+                            <Nav.Link href="#link">Blog</Nav.Link>
+                            <Nav.Link href="#link">Faq's</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 
