f1959e38b7fb2001fd6d09ab4a8393f54f1c393e
vikrantcisdev <vikrant.raj@cisinlabs.com>
2025-01-14 18:46:27 +0530

    image update

--- src/components/ui/BottomBar.jsx
+++ src/components/ui/BottomBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from '../../assets/Images/Home.png';
 import Categories from '../../assets/Images/Categories.png';
 import Vendors from '../../assets/Images/Vendors.png';
@@ -7,6 +7,8 @@ import Account from '../../assets/Images/Account.png';
 
 const BottomBar = () => {
 
+    const [activeNav, setActiveNav] = useState("Home")
+
     const itemList= [
         {img:Home,title:"Home"},
         {img:Categories,title:"Categories"},
@@ -14,6 +16,9 @@ const BottomBar = () => {
         {img:Favourites,title:"Favourites"},
         {img:Account,title:"Account"},
     ]
+    useEffect(()=>{
+        setActiveNav("Home")
+    },[])
 
   return (
     <>
@@ -21,7 +26,7 @@ const BottomBar = () => {
         {
             itemList.map((item,index)=>{
                 return (
-                    <div className="icontext" key={index}>
+                    <div className={`icontext ${item.title===activeNav ? "active" : ""}`} key={index}>
                         <img src={item.img} alt={item.title} className='img-fluid' />
                         <p>{item.title}</p>
                     </div>
