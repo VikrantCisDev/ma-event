15d0f518b92ae3ad9796255edb3f0135b082d073
rajnish888 <rajnish.p@cisinlabs.com>
2025-02-05 17:44:12 +0530

    created MultiMenus

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,126 +1,143 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from '../../assets/Images/logo.svg';
-import { HiMagnifyingGlass } from "react-icons/hi2";
-import { ImFacebook2 } from "react-icons/im";
-import { FaUserCircle, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
-import MegaMenu from './MegaMenu';
-import { Container } from 'react-bootstrap';
-import { IoMdArrowDropdown } from "react-icons/io";
-import { FaPlus } from "react-icons/fa";
+import { Container, Nav, NavDropdown } from 'react-bootstrap';
+import { FaPlus, FaUserCircle } from "react-icons/fa";
 import { HiOutlineUser } from "react-icons/hi2";
 import { IoChatbubblesOutline } from "react-icons/io5";
+import { IoMdArrowDropdown } from "react-icons/io";
 import { GiHamburgerMenu } from "react-icons/gi";
+import MegaMenu from './MegaMenu';
 import MultilevelMenu from './MultiMenus';
 import { Link } from 'react-router-dom';
+import logo from '../../assets/Images/logo.svg';
 
 const Header = () => {
-    const [active, setActive] = useState(false);
+  // State for mobile navigation toggle
+  const [active, setActive] = useState(false);
+  // State for multi-level sidebar
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
-    const handleClick = () => {
-      setActive(!active);
+  // Toggle mobile navigation
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  // Close all menus
+  const handleClose = () => {
+    setActive(false);
+    setSidebarOpen(false);
+  };
+
+  // Sidebar toggle handler
+  const handleSidebarToggle = (isOpen) => {
+    setSidebarOpen(isOpen);
+  };
+
+  useEffect(() => {
+    document.body.style.overflow = active ? "hidden" : "";
+    return () => {
+      document.body.style.overflow = "";
     };
-  
-    const handleClose = () => {
-      setActive(false); // Ensure the menu is closed
-    };
+  }, [active]);
 
-    
-    useEffect(() => {
-        if (active) {
-          document.body.style.overflow = "hidden"; 
-        } else {
-          document.body.style.overflow = "";
-        }
+  return (
+    <header className='header'>
+      <Container className="custom-container">
+        <Navbar expand="lg" className="headerNavbar">
+          <Navbar.Brand>
+            <Link to='/'><img className='img-fluid logo' src={logo} alt="Ma-event" /></Link>
+          </Navbar.Brand>
+          <div className='d-flex align-items-center gap-2'>
+            <div className="nav-mega d-lg-none d-flex gap-2 align-items-center">
+              <div className="nav-mega">
+                <a href="#" className='rightNavLink postYourAd'>
+                  <FaPlus /> Post <span className='postSpan'> your</span> AD
+                </a>
+              </div>
+              <div>
+                <a href="#" className='rightNavLink p-0'>
+                  <IoChatbubblesOutline color='#921f62' size={30} />
+                </a>
+              </div>
+              <NavDropdown
+                title={<HiOutlineUser color='#921f62' size={30} />}
+                className='myAccount'
+                id="basic-nav-dropdown"
+                as="div"
+              >
+                <NavDropdown.Toggle as="a" className="nav-link p-0">
+                  <HiOutlineUser color='#921f62' size={30} />
+                </NavDropdown.Toggle>
+                <NavDropdown.Menu>
+                  <NavDropdown.Item href="#action/3.3">Login</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Register</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">My Ads</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Favourites</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Messages</NavDropdown.Item>
+                </NavDropdown.Menu>
+              </NavDropdown>
 
-        return () => {
-          document.body.style.overflow = "";
-        };
-      }, [active]);
+            </div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleClick}>
+              <GiHamburgerMenu color='#921f62' size={30} />
+            </Navbar.Toggle>
+          </div>
+          <Navbar className={`responsiveNav ${active ? 'active' : ''}`} id="basic-navbar-nav">
+            <Nav className="center-links text-uppercase d-lg-flex d-none">
+              <Nav.Link href="#home">About ma-event</Nav.Link>
+              <div className="nav-mega">
+                <Link to="/vendorpage" className='nav-link'>Vendors <IoMdArrowDropdown /></Link>
+                <div className="megamenu-wrapper">
+                  <MegaMenu />
+                </div>
+              </div>
+              <Nav.Link href="#link">Events ping</Nav.Link>
+              <div className="nav-mega">
+                <Nav.Link href="#">Planning tools <IoMdArrowDropdown /></Nav.Link>
+                <div className="megamenu-wrapper">
+                  <MegaMenu />
+                </div>
+              </div>
+              <Nav.Link href="#link">Blog</Nav.Link>
+              <Nav.Link href="#link">Help</Nav.Link>
+              <div className="nav-mega">
+                <a href="#" className='rightNavLink'>
+                  <FaUserCircle size={30} color='black' /> My Account
+                </a>
+                <div className="megamenu-wrapper menu-list">
+                  <ul className='m-0 p-0 list-style-none'>
+                    <li><a href="">Login</a></li>
+                    <li><a href="">Register</a></li>
+                    <li><a href="">My Ads</a></li>
+                    <li><a href="">Favourites</a></li>
+                    <li><a href="">Profile</a></li>
+                    <li><a href="">Messages</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="nav-mega">
+                <a href="#" className='rightNavLink postYourAd'>
+                  <FaPlus /> Post <span className='postSpan'> your</span> AD
+                </a>
+              </div>
+            </Nav>
 
-    return (
-        <header className='header'>
-            <Container className="custom-container">
-                <Navbar expand="lg" className={` headerNavbar`}>
-                    <Navbar.Brand href="#home">
-                        <Link to='/'><img className='img-fluid logo' src={logo} alt="Ma-event" /></Link>
-                    </Navbar.Brand>
-                    <div className='d-flex align-items-center gap-2'>
-                        <div className="nav-mega d-lg-none d-flex gap-2 align-items-center">
-                            <div className="nav-mega">
-                                <a href="#" className='rightNavLink postYourAd '><FaPlus /> Post <span className='postSpan'> your</span> AD</a>
-                            </div>
-                            <div>
-                                <a href="#" className='rightNavLink p-0'><IoChatbubblesOutline color='#921f62' size={30} /></a>
-                            </div>
-                            <NavDropdown title={<><HiOutlineUser color='#921f62' size={30} /></>} className='myAccount' id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.3">Login</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Register</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">My Ads</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Favourites</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Messages</NavDropdown.Item>
-                            </NavDropdown>
-                        </div>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleClick}>
-                            <GiHamburgerMenu color='#921f62' size={30} />
-                        </Navbar.Toggle>
-                    </div>
-                    <Navbar className={`responsiveNav ${active ? 'active' : ''}`} id="basic-navbar-nav">
-                        <Nav className="center-links text-uppercase d-lg-flex d-none">
-                            <Nav.Link href="#home">About ma-event</Nav.Link>
-                            <div className="nav-mega">
-                                <Link to="/vendorpage" className='nav-link'>Vendors <IoMdArrowDropdown /></Link>
-                                <div className="megamenu-wrapper">
-                                    <MegaMenu />
-                                </div>
-                            </div>
-                            <Nav.Link href="#link">Events ping</Nav.Link>
-                            <div className="nav-mega">
-                                <Nav.Link href="#">Planning tools <IoMdArrowDropdown /></Nav.Link>
-                                <div className="megamenu-wrapper">
-                                    <MegaMenu />
-                                </div>
-                            </div>
-                            <Nav.Link href="#link">Blog</Nav.Link>
-                            <Nav.Link href="#link">Help</Nav.Link>
-                            <div className="nav-mega">
-                                <a href="#" className='rightNavLink'><FaUserCircle size={30} color='black' /> My Account</a>
-                                <div className="megamenu-wrapper menu-list">
-                                    <ul className='m-0 p-0 list-style-none'>
-                                        <li><a href="">Login</a></li>
-                                        <li><a href="">Register</a></li>
-                                        <li><a href="">My Ads</a></li>
-                                        <li><a href="">Favourites</a></li>
-                                        <li><a href="">Profile</a></li>
-                                        <li><a href="">Messages</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="nav-mega">
-                                <a href="#" className='rightNavLink postYourAd'><FaPlus /> Post <span className='postSpan'> your</span> AD</a>
-                            </div>
-                        </Nav>
+            {/* Mobile Multi-Level Sidebar */}
+            <div className='d-lg-none mobileMultiMenu'>
+              <MultilevelMenu
+                open={sidebarOpen}
+                handleSidebarToggle={handleSidebarToggle}
+                handleClose={handleClose}
+              />
+            </div>
+          </Navbar>
+        </Navbar>
+      </Container>
 
-
-                        <div className='d-lg-none mobileMultiMenu'>
-                            <MultilevelMenu />
-                        </div>
-                    </Navbar>
-                </Navbar>
-            </Container>
-
-            <div className={`${active ? 'fade show' : ''}  offcanvas-backdrops `} onClick={handleClose}></div>
-        </header>
-
-
-    );
+      <div className={`${active ? 'fade show' : ''} offcanvas-backdrops`} onClick={handleClose}></div>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/ui/MultiMenus.jsx
+++ src/components/ui/MultiMenus.jsx
@@ -1,177 +1,137 @@
 import React, { useState } from "react";
-import { IoMdArrowDropdown } from "react-icons/io";
+import { LiaAngleRightSolid } from "react-icons/lia";
+import { LiaAngleLeftSolid } from "react-icons/lia";
+import { IoMdClose } from "react-icons/io";
+
+// Options structure with the icons uncommented
+const options = [
+  {
+    id: 1,
+    name: "POPULAR CATEGORIES",
+    icon: <LiaAngleRightSolid />,
+    children: [
+      { id: 1.1, name: "Events Venues" },
+      { id: 1.2, name: "Decorators / Decor" },
+      { id: 1.3, name: "Photographers" },
+      { id: 1.4, name: "Car Hires" },
+      { id: 1.5, name: "Beauty Salon" },
+      { id: 1.6, name: "Honeymoon Destinations" },
+      { id: 1.7, name: "Accommodation" },
+      { id: 1.8, name: "Wedding Planners" },
+      { id: 1.9, name: "Cakes" },
+      { id: 1.10, name: "Catering" },
+      { id: 1.11, name: "Videographers" },
+      { id: 1.12, name: "Florists" },
+      { id: 1.13, name: "Event Loans" },
+      { id: 1.14, name: "Wedding Websites" },
+    ],
+  },
+  {
+    id: 2,
+    name: "BRIDE & GROOM FAVOURITES",
+    icon: <LiaAngleRightSolid />,
+    children: [
+      { id: 2.1, name: "Wedding Dress" },
+      { id: 2.2, name: "Tuxed Outfits" },
+      { id: 2.3, name: "Bridal Wear" },
+      { id: 2.4, name: "Groom's Wear" },
+      { id: 2.5, name: "Gifts & Packaging" },
+      { id: 2.6, name: "Bridal Accessories" },
+      { id: 2.7, name: "Bridal Shoes" },
+    ],
+  },
+  {
+    id: 3,
+    name: "ENTERTAINMENT",
+    icon: <LiaAngleRightSolid />,
+    children: [
+      { id: 3.1, name: "Cultural Dance Groups" },
+      { id: 3.2, name: "Dance Bands" },
+      { id: 3.3, name: "Live Bands" },
+      { id: 3.4, name: "Ceremony Musicians" },
+      { id: 3.5, name: "DJs" },
+      { id: 3.6, name: "Choirs" },
+    ],
+  },
+  {
+    id: 4,
+    name: "ACCESSORIES",
+    icon: <LiaAngleRightSolid />,
+    children: [
+      { id: 4.1, name: "Jewellery" },
+      { id: 4.2, name: "Wedding Rings" },
+    ],
+  },
+  {
+    id: 5,
+    name: "EVENT RENTALS",
+    icon: <LiaAngleRightSolid />,
+    children: [
+      { id: 5.1, name: "PA Systems" },
+      { id: 5.2, name: "Tents & Chairs" },
+      { id: 5.3, name: "Warming Dishes" },
+      { id: 5.4, name: "Wedding Fonts" },
+      { id: 5.5, name: "Toast Master or MC" },
+    ],
+  },
+];
+
+const MultiMenus = ({ open, handleSidebarToggle, handleClose }) => {
+  const [currentMenu, setCurrentMenu] = useState(null); // State to handle current menu
+ 
+
+  // Handle parent menu click
+  const handleMenuClick = (menu) => {
+    setCurrentMenu(menu); // Set currentMenu to the clicked menu
+  };
+
+  // Handle back button click (go to the previous level)
+  const handleBack = () => {
+    setCurrentMenu(null); // Set currentMenu to null to go back to the root menu
+  };
 
 
-import { RxCross2 } from "react-icons/rx";
+  return (
+    <div className={`sidebar ${open ? "open" : ""}`}>
+      <div className="sidebar-header">
+        {currentMenu ? (
+          <button className="back-btn p-0" onClick={handleBack}>
+            <LiaAngleLeftSolid /> Back
+          </button>
+        ) : (
+          <button className="close-btn p-0" onClick={handleClose}>
+            <IoMdClose size={22} />
+          </button>
+        )}
+      </div>
 
-const MultilevelMenu = () => {
-  const menuData = [
-    {
-      label: "ABOUT MA-EVENT",
-    },
-    {
-      label: "VENDORS",
-      subItems: [
-        {
-          label: "POPULAR CATEGORIES",
-          subItems: [
-            { label: "Events Venues" },
-            { label: "Decorators / Decor" },
-            { label: "Photographers" },
-            { label: "Car Hires" },
-            { label: "Beauty Salon" },
-            { label: "Honeymoon Destinations" },
-            { label: "Accommodation" },
-            { label: "Wedding Planners" },
-            { label: "Cakes" },
-            { label: "Catering" },
-            { label: "Videographers" },
-            { label: "Florists" },
-            { label: "Event Loans" },
-            { label: "Wedding Websites" },
-          ],
-        },
-        {
-          label: "BRIDE & GROOM FAVOURITES",
-          subItems: [
-            { label: "Wedding Dress" },
-            { label: "Tuxed Outfits" },
-            { label: "Bridal Wear" },
-            { label: "Groom's Wear" },
-            { label: "Gifts & Packaging" },
-            { label: "Bridal Accessories" },
-            { label: "Bridal Shoes" },
-          ],
-        },
-        {
-          label: "ENTERTAINMENT",
-          subItems: [
-            { label: "Cultural Dance Groups" },
-            { label: "Dance Bands" },
-            { label: "Live Bands" },
-            { label: "Ceremony Musicians" },
-            { label: "DJs" },
-            { label: "Choirs" },
-          ],
-        },
-        {
-          label: "ACCESSORIES",
-          subItems: [{ label: "Jewellery" }, { label: "Wedding Rings" }],
-        },
-        {
-          label: "EVENT RENTALS",
-          subItems: [
-            { label: "PA Systems" },
-            { label: "Tents & Chairs" },
-            { label: "Warming Dishes" },
-            { label: "Wedding Fonts" },
-            { label: "Toast Master or MC" },
-          ],
-        },
-      ],
-    },
-    { label: "EVENT PING" },
-    {
-      label: "PLANNING TOOLS",
-      subItems: [
-        {
-          label: "POPULAR CATEGORIES",
-          subItems: [
-            { label: "Events Venues" },
-            { label: "Decorators / Decor" },
-            { label: "Photographers" },
-            { label: "Car Hires" },
-            { label: "Beauty Salon" },
-            { label: "Honeymoon Destinations" },
-            { label: "Accommodation" },
-            { label: "Wedding Planners" },
-            { label: "Cakes" },
-            { label: "Catering" },
-            { label: "Videographers" },
-            { label: "Florists" },
-            { label: "Event Loans" },
-            { label: "Wedding Websites" },
-          ],
-        },
-        {
-          label: "BRIDE & GROOM FAVOURITES",
-          subItems: [
-            { label: "Wedding Dress" },
-            { label: "Tuxed Outfits" },
-            { label: "Bridal Wear" },
-            { label: "Groom's Wear" },
-            { label: "Gifts & Packaging" },
-            { label: "Bridal Accessories" },
-            { label: "Bridal Shoes" },
-          ],
-        },
-        {
-          label: "ENTERTAINMENT",
-          subItems: [
-            { label: "Cultural Dance Groups" },
-            { label: "Dance Bands" },
-            { label: "Live Bands" },
-            { label: "Ceremony Musicians" },
-            { label: "DJs" },
-            { label: "Choirs" },
-          ],
-        },
-        {
-          label: "ACCESSORIES",
-          subItems: [{ label: "Jewellery" }, { label: "Wedding Rings" }],
-        },
-        {
-          label: "EVENT RENTALS",
-          subItems: [
-            { label: "PA Systems" },
-            { label: "Tents & Chairs" },
-            { label: "Warming Dishes" },
-            { label: "Wedding Fonts" },
-            { label: "Toast Master or MC" },
-          ],
-        },
-      ],
-    },
-    { label: "BLOG" },
-    { label: "HELP" }
-  ];
+      <div className="sidebar-content">
+        {currentMenu ? (
+          // Rendering child items of the current menu
+          <div>
+            <ul>
+              {currentMenu.children.map((child) => (
+                <li key={child.id}>
+                  {child.name}
+                 
+                </li>
+              ))}
+            </ul>
 
-
-  const MenuItem = ({ item }) => {
-    const [isOpen, setIsOpen] = useState(false);
-
-    const toggleSubMenu = (e) => {
-      e.stopPropagation();
-      setIsOpen(!isOpen);
-    };
-
-    return (
-      <li onClick={toggleSubMenu} className={`${isOpen ? "nav-active" : ""}`}>
-        <a href="#">{item.label}{item.subItems && (<IoMdArrowDropdown />)}</a>
-        {item.subItems && (
-          <ul className={`d-${isOpen ? "block" : "none"} submenu ${isOpen ? "submenu-open" : ""}`}>
-            {item.subItems.map((subItem, index) => (
-              <MenuItem key={index} item={subItem} />
+          </div>
+        ) : (
+          // Rendering the parent menus
+          <ul className="main-navList">
+            {options.map((menu) => (
+              <li key={menu.id} onClick={() => handleMenuClick(menu)}>
+                {menu.icon} {menu.name}
+              </li>
             ))}
           </ul>
         )}
-      </li>
-    );
-  };
-
-  return (
-    <>
-
-      <div className  >
-        <RxCross2 size={25} />
       </div>
-      <ul style={{ listStyle: "none", padding: "0" }}>
-        {menuData.map((item, index) => (
-          <MenuItem key={index} item={item} />
-        ))}
-      </ul>
-    </>
+    </div>
   );
 };
 
-export default MultilevelMenu;
+export default MultiMenus;
